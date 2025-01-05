d6fb005777dbe817fd29d3e97d788d4cddb23d18
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2025-01-05 16:29:28 +0530

    Added Controller and cspoj

---
+++ randomdotNET/Controller/ProductCategoryController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using randomdotNET.Services;
+
+namespace randomdotNET.Controller
+{
+    [Route("api/productcategory")]
+    [ApiController]
+    public class ProductCategoryController(IProductService productService, ICategoryService categoryService)
+        : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetProductCategories()
+        {
+            var products = productService.GetAllProducts();
+            var categories = categoryService.GetAllCategories();
+
+            var query = from produt in products
+                join category in categories
+                    on produt.CategoryId equals category.CategoryId
+                select new { produt, category };
+
+            return Ok(query);    // query is executed here, Ok() serialize the data in json.
+        }
+    }
+}
